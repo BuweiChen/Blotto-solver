c1f3f2c64adca42bd2ea9c12051e786e71889910
Buwei Chen <59496117+BuweiChen@users.noreply.github.com>
2024-03-12 00:14:05 -0500

    Implement get_payoff_matrix

--- main.py
+++ main.py
@@ -66,9 +66,50 @@ def get_expected_value_general_strategies(g_strategy1, g_strategy2, values):
     
     return expected_value
 
-gs1 = [[1,1,3, 0.7],
-       [5,0,0, 0.3]]
-gs2 = [[3,2,0, 0.5],
-       [3,1,1, 0.4],
-       [0,0,5, 0.1]]
-print(get_expected_value_general_strategies(gs1, gs2, [5,2,1]))
+def get_payoff_matrix(units, values):
+    """gets the payoff matrix of the blotto game associated with units and values
+
+    Args:
+        units (int): the number of units to distribute for each player
+        values (list): a list of values of each battlefield
+
+    Returns:
+        list: the payoff matrix
+    """
+    pure_strategies = get_pure_strategies(units, values)
+    payoff_matrix = []
+    num_pure_strats = len(pure_strategies)
+    for i in range(num_pure_strats):
+        payoff_matrix_row = []
+        for j in range(num_pure_strats):
+            payoff_matrix_row.append(get_expected_value_pure_strategies(pure_strategies[i], pure_strategies[j], values))
+        payoff_matrix.append(payoff_matrix_row)
+    
+    return payoff_matrix
+
+def get_equilibrium_general_strategy(units, values):
+    """gets the nash equilibrium general strategy that is optimized for win or score
+
+    Args:
+        units (int): the number of units to distribute for each player
+        values (list): a list of values of each battlefield
+
+    Returns:
+        list: a list where each entry is a pure strategy followed by the probability of playing that strategy
+    """
+    pure_strategies = get_pure_strategies(units, values)
+    
+    # the nash general strategy must be the best strategy going up against itself
+    # it must be at least as good as any pure strategy going up against the nash general strategy
+    # let z = expected_value_general_strategies(nash_gstrat, nash_gstrat, values)
+    # we must have expected_value_general_strategies(pstrat, nash_gstrat, values) <= z
+    # where pstrat is any pure strategy
+    
+print(get_payoff_matrix(2, [1,2,3]))
+
+[[3.0, 3.5, 3.5, 4.0, 3, 4.0], 
+ [2.5, 3.0, 3.5, 3.5, 4.0, 5], 
+ [2.5, 2.5, 3.0, 2, 3.5, 3.5], 
+ [2.0, 2.5, 4, 3.0, 3.5, 4.0], 
+ [3, 2.0, 2.5, 2.5, 3.0, 3.5], 
+ [2.0, 1, 2.5, 2.0, 2.5, 3.0]]
