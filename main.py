189d5299e5cedf5062c4e73da556d8554041e669
Buwei Chen <59496117+BuweiChen@users.noreply.github.com>
2024-03-11 23:38:43 -0500

    Implement get_expected_value_general_strategies

--- main.py
+++ main.py
@@ -23,8 +23,7 @@ def get_pure_strategies(units, values):
     return pure_strategies
 
 def get_expected_value_pure_strategies(strategy1, strategy2, values):
-    """ get the expected value for player with strategy1 when going up against someone
-        using strategy2
+    """ get the expected value for player with strategy1 when going up against someone using strategy2
 
     Args:
         strategy1 (list): strategy of player 1
@@ -43,6 +42,33 @@ def get_expected_value_pure_strategies(strategy1, strategy2, values):
         elif strategy1[i] == strategy2[i]:
             p1_curr_battle_outcome = values[i]/2
         expected_value += p1_curr_battle_outcome
+        
     return expected_value
 
-print(get_expected_value_pure_strategies([1,2,2], [3,2,0], [1,2,3]))
+def get_expected_value_general_strategies(g_strategy1, g_strategy2, values):
+    """get the expected value of player with g_strategy1 when going up against someone using gstrategy2
+
+    Args:
+        g_strategy1 (list): a list where each entry is a pure strategy followed by the probability of playing that strategy
+        g_strategy2 (list): a list where each entry is a pure strategy followed by the probability of playing that strategy
+        values (list): a list of values of each battlefield
+
+    Returns:
+        float: expected value of player 1 score
+    """
+    num_battlefields = len(values)
+    expected_value = 0
+    for pure_strategy1 in g_strategy1:
+        prob_of_p_strat_1 = pure_strategy1[num_battlefields] # retrieves last element of array
+        for pure_strategy2 in g_strategy2:
+            prob_of_p_strat_2 = pure_strategy2[num_battlefields]
+            expected_value += get_expected_value_pure_strategies(pure_strategy1, pure_strategy2, values) * prob_of_p_strat_1 * prob_of_p_strat_2
+    
+    return expected_value
+
+gs1 = [[1,1,3, 0.7],
+       [5,0,0, 0.3]]
+gs2 = [[3,2,0, 0.5],
+       [3,1,1, 0.4],
+       [0,0,5, 0.1]]
+print(get_expected_value_general_strategies(gs1, gs2, [5,2,1]))
